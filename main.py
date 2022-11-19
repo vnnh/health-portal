acef1d996af4bc51fe0f1e7721eced6f9ded84be
Joshua Chen <joshuachen.04@gmail.com>
2022-11-19 09:21:26 -0600

    outline programming tasks

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
 if __name__ == "__main__":
-    return
+    # create a database
+    # using OCR to allow quick input of data
+    # streamlit website as ender user portal to access information
+        # On the website, have a prompt to allow doctors to share information with patients.
+    print("Hello World")
