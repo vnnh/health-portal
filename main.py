a68b8365b0f4f3bf1e12583b0cd2dae89cc99994
vnnh <90565423+vnnh@users.noreply.github.com>
2022-11-19 19:21:36 -0600

    add login page

--- main.py
+++ main.py
@@ -1,13 +1,37 @@
-import streamlit
+import streamlit as st
 
-def main():
-    return
+
+def check_password():
+    def password_entered():
+        if (
+            st.session_state["username"] in st.secrets["passwords"]
+            and st.session_state["password"]
+            == st.secrets["passwords"][st.session_state["username"]]
+        ):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]
+            del st.session_state["username"]
+        else:
+            st.session_state["password_correct"] = False
+
+    if "password_correct" not in st.session_state:
+        st.text_input("Username", on_change=password_entered, key="username")
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        return False
+    elif not st.session_state["password_correct"]:
+        st.text_input("Username", on_change=password_entered, key="username")
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        st.error("Password incorrect")
+        return False
+    else:
+        return True
+
 
 if __name__ == "__main__":
-    main()
-
-    # Tasks:
-    # create a database
-    # using OCR to allow quick input of data
-    # streamlit website as ender user portal to access information
-        # On the website, have a prompt to allow doctors to share information with patients.
+    if check_password():
+        #route
+        print("Hello world")
